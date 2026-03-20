eab784932d5bdfc58369e25f9615dbb814b72b8a
Elena Ventura Roldán <elenaventura38@gmail.com>
2026-03-20 10:03:53 +0100

    Add files model

--- app/files/models.py
+++ app/files/models.py
@@ -1,1 +1,15 @@
-# TODO: models tortoise for Files endpoints
+from tortoise import fields, models
+
+
+class StoredFile(models.Model):
+    id = fields.IntField(pk=True)
+    owner_external_id = fields.IntField()
+    filename = fields.CharField(max_length=255)
+    description = fields.TextField(null=True)
+    mime_type = fields.CharField(max_length=100, null=True)
+    content = fields.TextField(null=True)
+    created_at = fields.DatetimeField(auto_now_add=True)
+    updated_at = fields.DatetimeField(auto_now=True)
+
+    class Meta:
+        table = "stored_files"
