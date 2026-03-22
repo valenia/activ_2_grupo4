42f42d1921397215632930aff196e8af8a46c5a7
Juan Gabriel Carvajal Franco <juangracarvajal@gmail.com>
2026-03-22 13:32:58 +0100

    Implementada bbdd a authentication

--- app/authentication/models.py
+++ app/authentication/models.py
@@ -1,10 +1,10 @@
-# TODO: models tortoise for Authentication endpoints
-from tortoise import fields
-from tortoise .models import Model
+from tortoise import fields, models
 
-class UserDB(Model):
+
+class UserDB(models.Model):
     id = fields.IntField(pk=True)
-    username: fields.CharField(max_length=30, min_length = 3, unique = True)
-    email: fields.CharField(max_length=255, unique = True)
-    address: fields.CharField(max_length=255, null=True)
-    hashed_password: fields.CharField(max_length=255)
+    username = fields.CharField(max_length=30, min_length=3, unique=True)
+    email = fields.CharField(max_length=255, unique=True)
+    address = fields.CharField(max_length=255, null=True)
+    hashed_password = fields.CharField(max_length=255)
+    
